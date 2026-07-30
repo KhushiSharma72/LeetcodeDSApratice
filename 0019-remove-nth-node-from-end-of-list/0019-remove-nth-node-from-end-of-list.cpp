228707bc2fb8a52d73dbadc06c049255afb2cb32
KhushiSharma72 <149526448+KhushiSharma72@users.noreply.github.com>
2026-07-30 16:38:19 +0530

    Time: 0 ms (100%), Space: 14.8 MB (93.8%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -27,23 +27,6 @@ public:
         }
         temp->next=temp->next->next;
         return head;
-
-
-        // ListNode* dummy=new ListNode(0);
-        // dummy->next=head;
-        // ListNode* slow=dummy;
-        // ListNode* fast=dummy;
-
-        // for(int i=0;i<n;i++){
-        //     fast=fast->next;
-        // }
-
-        // while(fast->next!=nullptr){
-        //     fast=fast->next;
-        //     slow=slow->next;
-        // }
-        // slow->next=slow->next->next;
-        // return dummy->next;
-
+        
     }
 };
